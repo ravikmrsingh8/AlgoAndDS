74cbf42077a07cab10ed5f2e3a8a9154f02aef3b
Ravi Kumar Singh <RaviKumar.Singh@jda.com>
2017-12-27 16:09:19 +0530

    Readme

--- src/ReadmeUpdater.java
+++ src/ReadmeUpdater.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class ReadmeUpdater {
     private static final String _$ = File.separator;
     private static final Map<String, List<String>> files = new LinkedHashMap<>();
-    private static final String path = "src"+_$+"com"+_$+"example"+_$;
+    private static final String path = "src"+_$+"com"+_$+"example";
 
     public static void main(String[] args) throws IOException {
         parseDirectories();
@@ -17,14 +17,17 @@ public class ReadmeUpdater {
     private static void updateReadMe() throws IOException {
 
         BufferedWriter br = new BufferedWriter(new FileWriter("./README.md"));
+
         files.forEach((cat, files)->{
             try {
                 System.out.println("### "+cat);
                 br.write("### "+cat);
                 br.newLine();
+
                 for(int i=0; i<files.size(); i++) {
-                    System.out.println((i+1)+". ["+files.get(i)+"]("+path+cat+files.get(i)+")");
-                    br.write((i+1)+". ["+files.get(i)+"]("+path+cat+files.get(i)+")");
+                    String filePath = (path+_$+cat+_$+files.get(i)).replaceAll("\\\\", "/");
+                    System.out.println((i+1)+". ["+files.get(i)+"]("+filePath+")");
+                    br.write((i+1)+". ["+files.get(i)+"]("+filePath+")");
                     br.newLine();
                 }
                 ;
@@ -39,9 +42,11 @@ public class ReadmeUpdater {
         Files.walk(Paths.get(path)).filter(Files::isRegularFile).forEach((Path p) ->{
             String fileName = p.getFileName().toString();
             String pathStr = p.toString();
-            int index = pathStr.indexOf(_$+"example")+ 9;
+            int index = pathStr.indexOf(_$+"example"+_$)+ 9;
 
             String category = pathStr.substring(index, pathStr.indexOf(fileName)-1);
+            category.replace("\\", "/");
+
             List<String> jFiles =  files.get(category);
             if(jFiles == null) {
                 jFiles = new ArrayList<>();
