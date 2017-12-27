f1b03a70329c36b7445a7bf09fd094d7b8361b33
Ravi Kumar Singh <RaviKumar.Singh@jda.com>
2017-12-27 16:25:27 +0530

    Readme

--- src/ReadmeUpdater.java
+++ src/ReadmeUpdater.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class ReadmeUpdater {
     private static final String _$ = File.separator;
     private static final Map<String, List<String>> files = new LinkedHashMap<>();
-    private static final String path = "src"+_$+"com"+_$+"example";
+    private static final String path = "src" + _$ + "com" + _$ + "example";
 
     public static void main(String[] args) throws IOException {
         parseDirectories();
@@ -18,37 +18,38 @@ public class ReadmeUpdater {
 
         BufferedWriter br = new BufferedWriter(new FileWriter("./README.md"));
 
-        files.forEach((cat, files)->{
+        files.forEach((cat, files) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append("|" + cat + "|  |  |  |" + "\n" + "|-----------------|--|--|--|\n");
+            for (int i = 0; i < files.size(); i++) {
+                if (i % 4 == 0) sb.append("|");
+                String filePath = (path + _$ + cat + _$ + files.get(i)).replaceAll("\\\\", "/");
+                sb.append((i + 1) + ". [" + files.get(i) + "](" + filePath + ")" + "|");
+                if ((i + 1) % 4 == 0) sb.append("\n");
+            }
+            System.out.println(sb);
             try {
-                System.out.println("### "+cat);
-                br.write("### "+cat);
-                br.newLine();
-
-                for(int i=0; i<files.size(); i++) {
-                    String filePath = (path+_$+cat+_$+files.get(i)).replaceAll("\\\\", "/");
-                    System.out.println((i+1)+". ["+files.get(i)+"]("+filePath+")");
-                    br.write((i+1)+". ["+files.get(i)+"]("+filePath+")");
-                    br.newLine();
-                }
-                ;
+                br.write(sb.toString());
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         });
         br.flush();
         br.close();
     }
+
     private static void parseDirectories() throws IOException {
-        Files.walk(Paths.get(path)).filter(Files::isRegularFile).forEach((Path p) ->{
+        Files.walk(Paths.get(path)).filter(Files::isRegularFile).forEach((Path p) -> {
             String fileName = p.getFileName().toString();
             String pathStr = p.toString();
-            int index = pathStr.indexOf(_$+"example"+_$)+ 9;
+            int index = pathStr.indexOf(_$ + "example" + _$) + 9;
 
-            String category = pathStr.substring(index, pathStr.indexOf(fileName)-1);
+            String category = pathStr.substring(index, pathStr.indexOf(fileName) - 1);
             category.replace("\\", "/");
 
-            List<String> jFiles =  files.get(category);
-            if(jFiles == null) {
+            List<String> jFiles = files.get(category);
+            if (jFiles == null) {
                 jFiles = new ArrayList<>();
             }
             jFiles.add(fileName);
