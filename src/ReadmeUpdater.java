28bfa2fe08f567dff3716ee93041b5571a6fd3a1
Ravi Kumar Singh <RaviKumar.Singh@jda.com>
2017-12-31 00:37:29 +0530

    Readme

--- src/ReadmeUpdater.java
+++ src/ReadmeUpdater.java
@@ -8,7 +8,9 @@ public class ReadmeUpdater {
     private static final String _$ = File.separator;
     private static final Map<String, List<String>> files = new LinkedHashMap<>();
     private static final String path = "src" + _$ + "com" + _$ + "example";
-
+    private static final Set<String> categories = new HashSet<>(Arrays.asList( new String[]{
+            "Array", "List", "Graph", "Stack", "Queue", "Graph", "Tree", "Trie", "Java", "Design", "Heap", "String", "Deque" , "Sort", "Problems"
+    }));
     public static void main(String[] args) throws IOException {
         parseDirectories();
         updateReadMe();
@@ -20,17 +22,12 @@ public class ReadmeUpdater {
 
         files.forEach((cat, files) -> {
             StringBuilder sb = new StringBuilder();
-            sb.append("\r\n#### ");
-            String[] heading = cat.replaceAll("\\\\", " ").split(" ");
-            for(String word : heading) {
-                word = Character.toUpperCase(word.charAt(0))  + word.substring(1,word.length());
-                sb.append(word).append(" ");
-            }
-            sb.append("| \r\n");
+            sb.append("\r\n#### ").append(cat).append("| \r\n");
+
             sb.append("|");
             files.forEach(file->{
-                String filePath = (path + _$ + cat + _$ + file).replaceAll("\\\\", "/");
-                sb.append("[" + file + "](" + filePath + ")");
+                String filePath = file.replaceAll("\\\\", "/");
+                sb.append("[" + file.substring(file.lastIndexOf("\\")+1) + "](" + filePath + ")");
                 sb.append("|");
 
             });
@@ -52,16 +49,20 @@ public class ReadmeUpdater {
             String pathStr = p.toString();
             int index = pathStr.indexOf(_$ + "example" + _$) + 9;
 
-            String category = pathStr.substring(index, pathStr.indexOf(fileName) - 1);
-            category.replace("\\", "/");
+            String packages = pathStr.substring(index, pathStr.indexOf(fileName) - 1);
 
-            List<String> jFiles = files.get(category);
-            if (jFiles == null) {
-                jFiles = new ArrayList<>();
-            }
-            jFiles.add(fileName);
-            files.put(category, jFiles);
+            categories.forEach(cat->{
+                if(packages.contains(cat.toLowerCase())) {
+                    List<String> jFiles = files.get(cat);
+                    if (jFiles == null) {
+                        jFiles = new ArrayList<>();
+                    }
+                    jFiles.add(pathStr);
+                    files.put(cat, jFiles);
+                }
+            });
         });
+
     }
 
 }
